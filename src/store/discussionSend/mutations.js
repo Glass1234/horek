58aef1d632bdcaa1292c4752c97c39094fba3184
Glass1234 <stepaniuk2016@yandex.ru>
2024-02-27 11:48:12 +0300

    fix

--- src/store/discussionSend/mutations.js
+++ src/store/discussionSend/mutations.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function postTG(state, val) {
   try {
     const response = await axios.post(
-      "http://localhost:3000/api/duplicateContact",
+      "http://79.137.194.169:3001/api/duplicateContact",
       val
     );
     console.log(response);
